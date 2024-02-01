72ff351cbebcfcfc08d9a0d06e7b6a0ca7d5cceb
Igor <igor@cincatarina.sc.gov.br>
2024-02-01 10:16:47 -0300

    feat: ajusta layout.

--- src/App.tsx
+++ src/App.tsx
@@ -12,37 +12,39 @@ function App() {
     useDayCountInfo(service)
 
   return (
-    <main className="border px-2 md:px-auto py-16 rounded border-slate-200  mt-48 md:mt-12 space-y-12 mx-2 md:mx-auto max-w-4xl flex flex-col items-center">
-      <section className="flex flex-col items-center">
-        <Tally5 size="50" />
-        <h1 className="text-1xl md:text-2xl text-center md:text-start font-bold">
-          Dias praticando Ho&apos;oponopono
-        </h1>
-      </section>
+    <main className="md:flex-col flex justify-center items-center h-screen mx-2">
+      <div className="w-full border px-2 md:px-auto py-16 rounded border-slate-200 space-y-12 mx-2 md:mx-auto max-w-4xl flex flex-col items-center">
+        <section className="flex flex-col items-center">
+          <Tally5 size="50" />
+          <h1 className="text-1xl md:text-2xl text-center md:text-start font-bold">
+            Dias praticando Ho&apos;oponopono
+          </h1>
+        </section>
 
-      <section className="space-y-2">
-        <CardAccountantInfo
-          total={dayCountInfo.total}
-          lastDayHeld={dayCountInfo.lastDayHeld}
-        />
-        <div className="flex gap-2 w-full">
-          <Button className="w-full" onClick={saveDayCountInfo}>
-            <PlusCircle className="mr-1 h-4 w-4" />
-            Adicionar +1
-          </Button>
-
-          <AlertConfirm
-            title="Deseja realmente zerar as informações ?"
-            description="Esta ação é irreversível, seus dados serão perdidos."
-            onConfirm={resetInfoDay}
-          >
-            <Button variant="outline" className="w-full">
-              <MinusCircle className="mr-1 h-4 w-4" />
-              Zerar
+        <section className="space-y-2">
+          <CardAccountantInfo
+            total={dayCountInfo.total}
+            lastDayHeld={dayCountInfo.lastDayHeld}
+          />
+          <div className="flex gap-2 w-full">
+            <Button className="w-full" onClick={saveDayCountInfo}>
+              <PlusCircle className="mr-1 h-4 w-4" />
+              Adicionar +1
             </Button>
-          </AlertConfirm>
-        </div>
-      </section>
+
+            <AlertConfirm
+              title="Deseja realmente zerar as informações ?"
+              description="Esta ação é irreversível, seus dados serão perdidos."
+              onConfirm={resetInfoDay}
+            >
+              <Button variant="outline" className="w-full">
+                <MinusCircle className="mr-1 h-4 w-4" />
+                Zerar
+              </Button>
+            </AlertConfirm>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
